98278887a0c88446aa928646d1ed358145a968a0
ARASH ARDALAN <a.ardalan07@gmail.com>
2023-07-26 10:07:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Pop-up alert on Home page
+window.onload = function() {
+    alert("Welcome to My Website!");
+  };
+  
+  // Pop-up button on Products page
+  function showPopup() {
+    alert("This is a product pop-up.");
+  }
+  
